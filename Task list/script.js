0299958d1bd1f796b099ec782256dd3683759cd0
srtoz3 <stroz.ru3@mail.ru>
2021-01-27 21:15:42 +0200

    new function

--- Task list/script.js
+++ Task list/script.js
@@ -1,23 +1,64 @@
 
-var clock =document.getElementById('clock');
-function hexoClock(){
+var clock = document.getElementById('clock');
+function hexoClock() {
 	var time = new Date();
-	var h= time.getHours().toString();
-	var m= time.getMinutes().toString();
-	var y=time.getFullYear().toString();
-	var mounth=time.getMonth().toString();
-	var dat=time.getDate().toString();
-	if (h.length<2) {
-		h='0'+h;
+	var h = time.getHours().toString();
+	var m = time.getMinutes().toString();
+	var y = time.getFullYear().toString();
+	var mounth = time.getMonth().toString();
+	var dat = time.getDate().toString();
+	if (h.length < 2) {
+		h = '0' + h;
 	}
-	if (m.length<2) {
-		m='0'+m;
+	if (m.length < 2) {
+		m = '0' + m;
 	}
-	var clockString = h+':'+m;
-	var dateString =y+'/'+mounth+'/'+dat;
-	clock.textContent=clockString;
+	var clockString = h + ':' + m;
+	var dateString = y + '/' + mounth + '/' + dat;
+	clock.textContent = clockString;
 }
- $('.input2').click(function (e) { 
+$('.input2').click(function (e) {
 
- });
+});
 hexoClock();
+
+function tabActive() {
+	let tabNav = document.querySelectorAll('.quest-list-li'),
+		tabContent = document.querySelectorAll('.full'),
+		tabName;
+
+	tabContent.forEach(item => {
+		item.classList.add('full')
+		if (item.className === 'quest-full full block_0') {
+			item.classList.add('active')
+			item.classList.remove('full')
+		}
+	})
+
+	tabNav.forEach(item => {
+		if (item.className === 'quest-list-li block_0') {
+			item.classList.add('select')
+		}
+		item.addEventListener('click', selectTab)
+	})
+	function selectTab() {
+		tabNav.forEach(item => {
+			item.classList.remove('select')
+		});
+		this.classList.add('select')
+		tabName = this.getAttribute('data-tab-name');
+		selectTabContent(tabName)
+	}
+	function selectTabContent(tabName) {
+		tabContent.forEach(item => {
+			if (item.classList.contains(tabName)) {
+				item.classList.add('active')
+				item.classList.remove('full')
+			} else {
+				item.classList.remove('active')
+				item.classList.add('full')
+			}
+		})
+	}
+}
+tabActive()
---
+++ todo/src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <main></main>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "App",
+  components: {},
+};
+</script>
+
+<style lang="scss">
+</style>
