2c0d74699dd3f98cc2c8dafd9a8815ee155dfa6f
srtoz3 <stroz.ru3@mail.ru>
2021-02-10 21:13:58 +0200

    new bloger

--- Orbita/js/script.js
+++ Orbita/js/script.js
@@ -16,3 +16,46 @@ testWebP(function (support) {
     }
 });
 
+
+let btnWork = document.querySelector('.bloger__button'),
+    massage = document.querySelector('.bloger__massage'),
+    massageRow = document.querySelector('.bloger__massage-row')
+
+btnWork.addEventListener('click', event => {
+    if (event.target) {
+        fadeIn(massage, 'flex')
+    }
+})
+massageRow.addEventListener('click', event => {
+    event.stopPropagation();
+})
+massage.addEventListener('click', event => {
+
+    if (event.target) {
+        fadeOut(massage)
+    }
+})
+
+function fadeOut(el) {
+    el.style.opacity = 1;
+    (function fade() {
+        if ((el.style.opacity -= .1) < 0) {
+            el.style.display = "none";
+        } else {
+            requestAnimationFrame(fade);
+        }
+    })();
+};
+
+// ** FADE IN FUNCTION **
+function fadeIn(el, display) {
+    el.style.opacity = 0;
+    el.style.display = display || "block";
+    (function fade() {
+        var val = parseFloat(el.style.opacity);
+        if (!((val += .1) > 1)) {
+            el.style.opacity = val;
+            requestAnimationFrame(fade);
+        }
+    })();
+};
